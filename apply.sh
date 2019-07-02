de985277cd7c4f049a9af856eda52686cf7e77b6
Ubuntu <ubuntu@master-pankaj.us-central1-a.c.superb-cubist-243117.internal>
2019-07-02 07:17:31 +0000

    channel create error

--- apply.sh
+++ apply.sh
@@ -1,20 +1,9 @@
 # !bin/bash
-kubectl apply -f pvs/org1.yaml
-kubectl apply -f pvs/org2.yaml
+kubectl apply -f pvs/
 
-kubectl apply -f pvs/chaincode.yaml
-kubectl apply -f pvs/channel-artifacts.yaml
-kubectl apply -f pvs/cli.yaml
-kubectl apply -f pvs/orderer-crypto.yaml
-kubectl apply -f pvc/peer0-certificate.yaml
-kubectl apply -f pvc/peer0-org2-certificate.yaml
-kubectl apply -f pvc/chaincode-pvc.yaml
-kubectl apply -f pvc/channel-artifacts-pvc.yaml
-kubectl apply -f pvc/cli-certificate-pvc.yaml
-kubectl apply -f pvc/orderer-crypto-pvc.yaml
-kubectl apply -f org1-peers/peer0-pod.yaml
-kubectl apply -f org1-peers/peer0-org2-pod.yaml
-kubectl apply -f org1-peers/cli-pod.yaml
-kubectl apply -f org1-peers/orderer-pod.yaml
-kubectl apply -f services/oerder0-service.yaml
+kubectl apply -f pvc/
+
+kubectl apply -f deployments/
+
+
 
--- delete.sh
+++ delete.sh
@@ -1,19 +1,9 @@
 # !bin/bash
-kubectl delete -f org1-peers/peer0-pod.yaml
-kubectl delete -f org1-peers/peer0-org2-pod.yaml
-kubectl delete -f org1-peers/cli-pod.yaml
-kubectl delete -f org1-peers/orderer-pod.yaml
-kubectl delete -f services/oerder0-service.yaml
-kubectl delete -f pvc/peer0-certificate.yaml
-kubectl delete -f pvc/peer0-org2-certificate.yaml
-kubectl delete -f pvc/chaincode-pvc.yaml
-kubectl delete -f pvc/channel-artifacts-pvc.yaml
-kubectl delete -f pvc/cli-certificate-pvc.yaml
-kubectl delete -f pvc/orderer-crypto-pvc.yaml
-kubectl delete -f pvs/org1.yaml
-kubectl delete -f pvs/org2.yaml
-kubectl delete -f pvs/chaincode.yaml
-kubectl delete -f pvs/channel-artifacts.yaml
-kubectl delete -f pvs/cli.yaml
-kubectl delete -f pvs/orderer-crypto.yaml
+kubectl delete -f deployments/
+
+
+kubectl delete -f pvc/
+
+kubectl delete -f pvs/
+
 
